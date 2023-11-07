765dc1e302b5c340246db4b8a59eed6d958525ec
rachelhoneycutt <142539235+rachelhoneycutt@users.noreply.github.com>
2023-11-07 13:30:24 -0600

    Add files via upload

--- scripts/script.js
+++ scripts/script.js
@@ -1,10 +1,9 @@
-const teams = [];
-
 $(document).ready(function(){
-  $('.signUp').hide();  
-    
+  $('.signUp').hide();
+ // $('.tooltip').hide();
+
   $('#signUpBTN').click(function(){
-    $('.loginPage').hide();
+    $('.homePage').hide();
     $('.signUp').show(); 
   });
 
@@ -14,31 +13,70 @@ $(document).ready(function(){
 
   $('#submitBtn').click(function(){
     let userEmail = document.getElementById("newEmail").value;
-    let password = document.getElementById("newPwd").value;
+    let userPassword = document.getElementById("newPwd").value;
     let teamName = document.getElementById("team").value;
     let fname = document.getElementById("fname").value;
     let lname = document.getElementById("lname").value;
     let number = document.getElementById("phoneNumber").value;
     let teamNum = document.getElementById("num").value;
 
-    teams.push({userEmail, password, teamName, fname, lname, number, teamNum});
+    teams.push({userEmail, userPassword, teamName, fname, lname, number, teamNum});
 
-    $('.signUp').hide();
-    $('.loginPage').show(); 
+    if (userEmail == "" || userPassword == "" || teamName == "" || fname == "" || lname == "" || number == "" || teamNum == "") {
+      //$('.tooltip').show();
+      //$('.tooltip').addClass('show-tooltip');
+    } else {
+      $('.signUp').hide();
+      $('.homePage').show(); 
+    }
   });
     
   $("#login").click(function(){
-    // when they press login check username & password
+    // when they press login check email & password    
     // If login is valid dispay a welcome with the team name
+    let email = document.getElementById("oldEmail").value;
+    let password = document.getElementById("oldPwd").value;
+
+    if (email == "" || password == ""){
+
+    } else {
+      if(teams.length > 0){
+        for (let i = 0; i < teams.length; i++) {
+          if(email == teams[i].userEmail && password == teams[i].userPassword) {
+            welcomePage(i);
+          } else {
+            goSignUp();
+          }
+        }
+      } else {
+        goSignUp();
+      }
+    }
   })
 
 });
+
+function welcomePage(num) {
+  let home = document.getElementsByClassName("homePage");
+  home[0].innerHTML = `
+    <h2>Team Name: ` + teams[num].teamName + `</h2>
+    <h3>Captain: ` + teams[num].fname + ' ' + teams[num].lname + `</h3>
+    <h4>Number of Team Members: ` + teams[num].teamNum + `<h4>
+  `;
+};
+
+function goSignUp() {
+  let home = document.getElementsByClassName("homePage");
+  home[0].innerHTML = `
+    <h1>There is no team matching this login information. Please sign up your team before trying to login.</h1>
+  `;
+}
   
 $(function() {
   
   //click the eye icon w/class reveal 
   $(".reveal").on('click',function() {
-    let $pwd = $("#oldpwd");
+    let $pwd = $("#oldPwd");
     
     if ($pwd.attr('type') === 'password') {
         $pwd.attr('type', 'text');
